32c7261db3753276f5d7560b8018e76026155993
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-01-30 01:28:26 +0200

    GHA and SQLAlchemy, maybe this is the problem?

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,6 +6,4 @@ from src.app_factory import AppFactory
 @pytest.fixture
 def app():
     app = AppFactory().create_app(True)
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("TEST_DATABASE_URL")
     return app
